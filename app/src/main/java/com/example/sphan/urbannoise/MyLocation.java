a67bdd2dbc74a0033545f73ccfeb20a76015cbe8
sphan <santhai.phan@gmail.com>
2015-11-11 01:36:20 +1100

    get something working like what Adam had

---
+++ app/src/main/java/com/example/sphan/urbannoise/MyLocation.java
@@ -0,0 +1,195 @@
+package com.example.sphan.urbannoise;
+
+import android.location.Location;
+import android.location.LocationListener;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+
+public class MyLocation extends AppCompatActivity implements
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener, com.google.android.gms.location.LocationListener {
+
+    private static final String TAG = MyLocation.class.getSimpleName();
+    private static final long LOCATION_UPDATE_INTERVAL = 5 * 1000; // 5 milliseconds
+
+    private GoogleApiClient mGoogleApiClient;
+    private Location mLastLocation;
+    private Location mCurrentLocation;
+    private LocationRequest mLocationRequest;
+
+    private TextView latitudeTextView;
+    private TextView longitudeTextView;
+
+    private boolean mRequestingLocationUpdates;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_location);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        buildGoogleApiClient();
+
+        latitudeTextView = (TextView) findViewById(R.id.latitudeTextview);
+        longitudeTextView = (TextView) findViewById(R.id.longitudeTextview);
+        mRequestingLocationUpdates = true;
+
+        mGoogleApiClient.connect();
+
+        createLocationRequest();
+    }
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+
+        if (mGoogleApiClient.isConnected() == false)
+        {
+            mGoogleApiClient.connect();
+        }
+    }
+
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+
+        createLocationRequest();
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+
+        Log.i(TAG, "Location service connected");
+        mCurrentLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        Toast.makeText(this, "GoogleApiClient connected", Toast.LENGTH_LONG).show();
+
+        if (mCurrentLocation == null)
+        {
+            Log.i(TAG, "mCurrentLocation is null");
+        }
+
+        if (mGoogleApiClient.isConnected() == false)
+        {
+            Log.i(TAG, "mGoogleApiClient is not connected");
+        }
+
+        if (mLocationRequest == null)
+        {
+            Log.i(TAG, "mLocationRequest is null");
+        }
+
+        if (mCurrentLocation == null &&
+                (mGoogleApiClient.isConnected() && mLocationRequest != null))
+        {
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+        }
+        else
+        {
+            updateUI();
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.i(TAG, "Location services suspended. Please reconnect.");
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Toast.makeText(this, "Connection Failure : " + connectionResult.toString(), Toast.LENGTH_LONG).show();
+//        Toast.makeText(this, "Connection Failure : " + connectionResult.getErrorMessage(), Toast.LENGTH_SHORT).show();
+
+//        if (connectionResult.hasResolution()) {
+//            try {
+//                // Start an Activity that tries to resolve the error
+//                connectionResult.startResolutionForResult(this, CONNECTION_FAILURE_RESOLUTION_REQUEST);
+//            } catch (IntentSender.SendIntentException e) {
+//                e.printStackTrace();
+//            }
+//        } else {
+//            Log.i(TAG, "Location services connection failed with code " + connectionResult.getErrorCode());
+//        }
+    }
+
+    protected void createLocationRequest() {
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(LOCATION_UPDATE_INTERVAL);
+        mLocationRequest.setFastestInterval(LOCATION_UPDATE_INTERVAL);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        mCurrentLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        Log.d(TAG, "my location: " + mCurrentLocation.toString());
+        updateUI();
+    }
+
+//    @Override
+//    public void onStatusChanged(String provider, int status, Bundle extras) {
+//
+//    }
+//
+//    @Override
+//    public void onProviderEnabled(String provider) {
+//
+//    }
+//
+//    @Override
+//    public void onProviderDisabled(String provider) {
+//
+//    }
+
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+
+        if (mGoogleApiClient.isConnected() == true)
+        {
+            LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient,
+                    (com.google.android.gms.location.LocationListener) this);
+            mGoogleApiClient.disconnect();
+        }
+    }
+
+    private void updateUI() {
+        if (mCurrentLocation != null)
+        {
+            latitudeTextView.setText(String.valueOf(mCurrentLocation.getLatitude()));
+            longitudeTextView.setText(String.valueOf(mCurrentLocation.getLongitude()));
+        }
+        else
+        {
+            Log.d(TAG, "my location is null");
+        }
+
+//        mLastUpdateTimeTextView.setText(mLastUpdateTime);
+    }
+}
